dcde5b1484e7a337cb24982981c3db0e056ecb71
vaibhav sharma <vaibhav.mitrc@gmail.com>
2023-07-04 22:13:09 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,8 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 from dataclasses import dataclass#it si used to create class variables
 
 @dataclass# it is a decorator which help us to define the class variable in this class without using __init__ function
@@ -44,7 +46,10 @@ class DataIngestion:
 
 if __name__=='__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
         
 
 
